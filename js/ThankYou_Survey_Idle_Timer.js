6359351566cabf40932aab691eed74449365e62b
Jennifer Hoitenga <jhoitenga@my365.bellevue.edu>
2022-12-06 06:40:27 -0600

    Update ThankYou_Survey_Idle_Timer.js

--- js/ThankYou_Survey_Idle_Timer.js
+++ js/ThankYou_Survey_Idle_Timer.js
@@ -1,1 +1,21 @@
+//global variable for timer
+var idleTimer;
 
+//start up
+$(document).ready(function() {
+    resetIdleTimer();
+});
+
+//reset timer
+$('body').on('click touchstart keydown scroll', 'body', function() {
+    resetIdleTimer();
+});
+
+//function for reset. Time is in ms (5 seconds)
+function resetIdleTimer() {
+    clearTimeout(idleTimer);
+    idleTimer = setTimeout(function() {
+        //exit survey here
+        window.location.href = 'ENTER_HTML_SURVEY_LANDING_PAGE_URL';
+    }, 4000);
+}
